3d151a616685c569340798487fe17227b25bd35c
Lyned <denilson.vasquez.lim@gmail.com>
2026-08-25 19:24:16 -0500

    Update enemy_data.js

--- data/Enemy/enemy_data.js
+++ data/Enemy/enemy_data.js
@@ -3,17 +3,17 @@
 const enemy = {
     "Tyrfing": {
         def_enemy: { Defense: 571.7 },
-        buffs_enemy: { Res_Ether: 0.3},
+        buffs_enemy: { Res_Ether: 0.2},
         Image: "static/ENEMY/Tyrfing.webp"
     },
     "Alpeca": {
-        def_enemy: { Defense: 921 },
-        buffs_enemy: { Res_Ether: 0.3},
+        def_enemy: { Defense: 571.7 },
+        buffs_enemy: { Res_Ether: 0.2},
         Image: "static/ENEMY/Alpeca.webp"
     },
     "Dullahan": {
         def_enemy: { Defense: 921 },
-        buffs_enemy: { Res_Ether: 0.3},
+        buffs_enemy: { Res_Ether: 0.2},
         Image: "static/ENEMY/Dullahan.webp"
     },
     "Maton": {
