9d01119972e567a5d0b0941a814bbebade31df35
Lyned <denilson.vasquez.lim@gmail.com>
2026-08-25 20:04:48 -0500

    Update dps_data.js

--- data/Characters/dps_data.js
+++ data/Characters/dps_data.js
@@ -427,7 +427,7 @@ const dps = {
             }
         },
         stat_escalado: {
-            stat_fuente: "Tasa+(1*Tasax)+Tasae",
+            stat_fuente: "Tasa*(1+Tasax)+Tasae",
             umbral: 140,
             stat_buff: [
                 {
