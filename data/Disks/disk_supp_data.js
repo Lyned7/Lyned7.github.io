e8087c20e9973357d20cc54de03ab00f7b04884c
Lyned <denilson.vasquez.lim@gmail.com>
2026-08-28 17:38:10 -0500

    Expand disk_supp_data with new disk entries
    Added new disk entries with images and buffs.

--- data/Disks/disk_supp_data.js
+++ data/Disks/disk_supp_data.js
@@ -1,4 +1,8 @@
 const disk_supp = {
+    "Empty": { 
+        Image: "static/DISKS/Set_Base.webp",
+        buffs: {}
+    },
     "King of the Summit": { 
         Image: "static/DISKS/King_of_the_Summit.webp",
         buffs: { CD: 0.3 }
@@ -6,6 +10,34 @@ const disk_supp = {
     "Moonlight Lullaby": {
         Image: "static/DISKS/Moonlight_Lullaby.webp",
         buffs: { Dmg: 0.18 }
+    },
+    "Bunny in Wonderland": {
+        Image: "static/DISKS/Bunny_in_Wonderland.webp",
+        buffs: { Dmg: 0.18 }
+    },
+    "Astral Voice": {
+        Image: "static/DISKS/Astral_Voice.webp",
+        buffs: { Dmg: 0.24 }
+    },
+    "Proto Punk": {
+        Image: "static/DISKS/Proto_Punk.webp",
+        buffs: { Dmg: 0.15 }
+    },
+    "Swing Jazz": {
+        Image: "static/DISKS/Swing_Jazz.webp",
+        buffs: { Dmg: 0.15 }
+    },
+    "Freedom Blues": {
+        Image: "static/DISKS/Freedom_Blues.webp",
+        buffs: {}
+    },
+    "Feathered Fate": {
+        Image: "static/DISKS/Feathered_Fates.webp",
+        buffs: {}
+    },
+    "Wuthering Salon": {
+        Image: "static/DISKS/Wuthering_Salon.webp",
+        buffs: {}
     },
 };
 
