cb911ea13490f420c537198a72c00d845e663470
Lyned <denilson.vasquez.lim@gmail.com>
2026-08-29 08:44:55 -0500

    Add files via upload

---
+++ data/Enemy/da.js
@@ -0,0 +1,21 @@
+const da = {
+    "[Buff 1]": {
+        buffs_da: {Dmg_Ultimate: 0.30, Dmg_Basic: 0.30, Res_Ice_Atacante: 0.20, Dmg_Ice: 0.30},
+        Image: "static/ENEMY/Buff1.webp"
+    },
+    "[Buff 2] - 2 Anomaly Agents": {
+        buffs_da: {Admg: 0.10, Atkf: 0.05, Res_Anomalo: 0.10, Shred_Anomalo: 0.10 },
+        Image: "static/ENEMY/Buff2.webp"
+    },
+    "[Buff 2] - 3 Anomaly Agents": {
+        buffs_da: {Admg: 0.30, Atkf: 0.15, Res_Anomalo: 0.10, Shred_Anomalo: 0.10},
+        Image: "static/ENEMY/Buff2.webp"
+    },
+    "[Buff 3]": {
+        buffs_da: {CD: 0.30, Atkf_Atacante: 0.10, Dmg_Basic: 0.30, Shred_Basic: 0.15},
+        Image: "static/ENEMY/Buff3.webp"
+    },
+    
+};
+
+export default da
