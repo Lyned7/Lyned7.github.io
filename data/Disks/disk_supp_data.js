87df320b1d95da2157d818a9babb7f54480dbeeb
Lyned <denilson.vasquez.lim@gmail.com>
2026-08-31 18:48:35 -0500

    Refactor disk_supp data structure
    Removed duplicate entries and cleaned up the disk_supp data structure.

--- data/Disks/disk_supp_data.js
+++ data/Disks/disk_supp_data.js
@@ -1,8 +1,4 @@
 const disk_supp = {
-    "Empty": { 
-        Image: "static/DISKS/Set_Base.webp",
-        buffs: {}
-    },
     "King of the Summit": { 
         Image: "static/DISKS/King_of_the_Summit.webp",
         buffs: { CD: 0.3 }
@@ -40,95 +36,95 @@ const disk_supp = {
         buffs: {}
     },
     "Branch & Blade Song": {
-        Image: "static/Disks/Branch_&_Blade_Song.webp",
+        Image: "static/DISKS/Branch_&_Blade_Song.webp",
         buffs: {}
     },
     "Chaos Jazz": {
-        Image: "static/Disks/Chaos_Jazz.webp",
+        Image: "static/DISKS/Chaos_Jazz.webp",
         buffs: {}
     },
     "Chaotic Metal": {
-        Image: "static/Disks/Chaotic_Metal.webp",
+        Image: "static/DISKS/Chaotic_Metal.webp",
         buffs: {}
     },
     "Dawns Bloom": {
-        Image: "static/Disks/Dawns_Bloom.webp",
+        Image: "static/DISKS/Dawns_Bloom.webp",
         buffs: {}
     },
     "Fanged Metal": {
-        Image: "static/Disks/Fanged_Metal.webp",
+        Image: "static/DISKS/Fanged_Metal.webp",
         buffs: {}
     },
     "Hormone Punk": {
-        Image: "static/Disks/Hormone_Punk.webp",
+        Image: "static/DISKS/Hormone_Punk.webp",
         buffs: {}
     },
     "Fanged Metal": {
-        Image: "static/Disks/Fanged_Metal.webp",
+        Image: "static/DISKS/Fanged_Metal.webp",
         buffs: {}
     },
     "Hormone Punk": {
-        Image: "static/Disks/Hormone_Punk.webp",
+        Image: "static/DISKS/Hormone_Punk.webp",
         buffs: {}
     },
     "Inferno Metal": {
-        Image: "static/Disks/Inferno_Metal.webp",
+        Image: "static/DISKS/Inferno_Metal.webp",
         buffs: {}
     },
     "Notes From the Chained": {
-        Image: "static/Disks/Notes_From_the_Chained.webp",
+        Image: "static/DISKS/Notes_From_the_Chained.webp",
         buffs: {}
     },
     "Phaethons Melody": {
-        Image: "static/Disks/Phaethons_Melody.webp",
+        Image: "static/DISKS/Phaethons_Melody.webp",
         buffs: {}
     },
     "Polar Metal": {
-        Image: "static/Disks/Polar_Metal.webp",
+        Image: "static/DISKS/Polar_Metal.webp",
         buffs: {}
     },
     "Puffer Electro": {
-        Image: "static/Disks/Puffer_Electro.webp",
+        Image: "static/DISKS/Puffer_Electro.webp",
         buffs: {}
     },
     "Shadow Harmony": {
-        Image: "static/Disks/Shadow_Harmony.webp",
+        Image: "static/DISKS/Shadow_Harmony.webp",
         buffs: {}
     },
     "Shining Aria": {
-        Image: "static/Disks/Shining_Aria.webp",
+        Image: "static/DISKS/Shining_Aria.webp",
         buffs: {}
     },
     "Shockstar Disco": {
-        Image: "static/Disks/Shockstar_Disco.webp",
+        Image: "static/DISKS/Shockstar_Disco.webp",
         buffs: {}
     },
     "Soul Rock": {
-        Image: "static/Disks/Soul_Rock.webp",
+        Image: "static/DISKS/Soul_Rock.webp",
         buffs: {}
     },
     "The Sky Ablaze": {
-        Image: "static/Disks/The_Sky_Ablaze.webp",
+        Image: "static/DISKS/The_Sky_Ablaze.webp",
         buffs: {}
     },
     "Thorned Rose": {
-        Image: "static/Disks/Thorned_Rose.webp",
+        Image: "static/DISKS/Thorned_Rose.webp",
         buffs: {}
     },
     "Thunder Metal": {
-        Image: "static/Disks/Thunder_Metal.webp",
+        Image: "static/DISKS/Thunder_Metal.webp",
         buffs: {}
     },
     "White Water Ballad": {
-        Image: "static/Disks/White_Water_Ballad.webp",
+        Image: "static/DISKS/White_Water_Ballad.webp",
         buffs: {}
     },
     "Woodpecker Electro": {
-        Image: "static/Disks/Woodpecker_Electro.webp",
+        Image: "static/DISKS/Woodpecker_Electro.webp",
         buffs: {}
     },
     "Yunkui Tales": {
-        Image: "static/Disks/Yunkui_Tales.webp",
+        Image: "static/DISKS/Yunkui_Tales.webp",
         buffs: {}
     },
 };
