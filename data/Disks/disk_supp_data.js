a7164d0122688dd3e397e32647bfcdb55295ef66
Lyned <denilson.vasquez.lim@gmail.com>
2026-08-31 18:31:15 -0500

    Expand disk_supp_data with additional disks
    Added new disk entries with images and buffs.

--- data/Disks/disk_supp_data.js
+++ data/Disks/disk_supp_data.js
@@ -39,6 +39,98 @@ const disk_supp = {
         Image: "static/DISKS/Wuthering_Salon.webp",
         buffs: {}
     },
+    "Branch & Blade Song": {
+        Image: "static/Disks/Branch_&_Blade_Song.webp",
+        buffs: {}
+    },
+    "Chaos Jazz": {
+        Image: "static/Disks/Chaos_Jazz.webp",
+        buffs: {}
+    },
+    "Chaotic Metal": {
+        Image: "static/Disks/Chaotic_Metal.webp",
+        buffs: {}
+    },
+    "Dawns Bloom": {
+        Image: "static/Disks/Dawns_Bloom.webp",
+        buffs: {}
+    },
+    "Fanged Metal": {
+        Image: "static/Disks/Fanged_Metal.webp",
+        buffs: {}
+    },
+    "Hormone Punk": {
+        Image: "static/Disks/Hormone_Punk.webp",
+        buffs: {}
+    },
+    "Fanged Metal": {
+        Image: "static/Disks/Fanged_Metal.webp",
+        buffs: {}
+    },
+    "Hormone Punk": {
+        Image: "static/Disks/Hormone_Punk.webp",
+        buffs: {}
+    },
+    "Inferno Metal": {
+        Image: "static/Disks/Inferno_Metal.webp",
+        buffs: {}
+    },
+    "Notes From the Chained": {
+        Image: "static/Disks/Notes_From_the_Chained.webp",
+        buffs: {}
+    },
+    "Phaethons Melody": {
+        Image: "static/Disks/Phaethons_Melody.webp",
+        buffs: {}
+    },
+    "Polar Metal": {
+        Image: "static/Disks/Polar_Metal.webp",
+        buffs: {}
+    },
+    "Puffer Electro": {
+        Image: "static/Disks/Puffer_Electro.webp",
+        buffs: {}
+    },
+    "Shadow Harmony": {
+        Image: "static/Disks/Shadow_Harmony.webp",
+        buffs: {}
+    },
+    "Shining Aria": {
+        Image: "static/Disks/Shining_Aria.webp",
+        buffs: {}
+    },
+    "Shockstar Disco": {
+        Image: "static/Disks/Shockstar_Disco.webp",
+        buffs: {}
+    },
+    "Soul Rock": {
+        Image: "static/Disks/Soul_Rock.webp",
+        buffs: {}
+    },
+    "The Sky Ablaze": {
+        Image: "static/Disks/The_Sky_Ablaze.webp",
+        buffs: {}
+    },
+    "Thorned Rose": {
+        Image: "static/Disks/Thorned_Rose.webp",
+        buffs: {}
+    },
+    "Thunder Metal": {
+        Image: "static/Disks/Thunder_Metal.webp",
+        buffs: {}
+    },
+    "White Water Ballad": {
+        Image: "static/Disks/White_Water_Ballad.webp",
+        buffs: {}
+    },
+    "Woodpecker Electro": {
+        Image: "static/Disks/Woodpecker_Electro.webp",
+        buffs: {}
+    },
+    "Yunkui Tales": {
+        Image: "static/Disks/Yunkui_Tales.webp",
+        buffs: {}
+    },
 };
 
 export default disk_supp
