e9842ed67b02107d4e14e08307b7ae8106c69edd
Lyned <denilson.vasquez.lim@gmail.com>
2026-09-10 21:31:21 -0500

    Add new enemies and update existing enemy data

--- data/Enemy/enemy_data.js
+++ data/Enemy/enemy_data.js
@@ -1,6 +1,30 @@
 // ENEMIES
 
 const enemy = {
+    "Phaeton": {
+        def_enemy: { Defense: 952 },
+        buffs_enemy: { Res_Electric: 0.2, Res_Physical: -0.2, Sdmg: 0.40},
+        buffs_stunned: { Stun: 0.20},
+        Image: "static/ENEMY/Phaeton.webp"
+    },
+    "Kusarikku": {
+        def_enemy: { Defense: 952 },
+        buffs_enemy: { Res_Electric: 0.2, Res_Fire: 0.2, Res_Ice: -0.2, Taken_Anomalo: -0.40, Sdmg: 0.40},
+        buffs_stunned: {},
+        Image: "static/ENEMY/Kusarikku.webp"
+    },
+    "Girtablullu": {
+        def_enemy: { Defense: 952 },
+        buffs_enemy: {Admg: 0.16, MA: 60},
+        buffs_stunned: {},
+        Image: "static/ENEMY/Girtablullu.webp"
+    },
+    "Ye Shiyuan": {
+        def_enemy: { Defense: 952 },
+        buffs_enemy: { Res_Wind: 0.2, Res_Ice: 0.2, Res_Physical: 0.2, Res_Electric: -0.2, Shred: 0.24},
+        buffs_stunned: { CD: 0.50},
+        Image: "static/ENEMY/YeShiyuan.webp"
+    },
     "Sweeper": {
         def_enemy: { Defense: 476 },
         buffs_enemy: { Res_Ice: 0.20, Res_Physical: -0.20, Res: 0.10, Atkf_Atacante: 0.20, Penx_Atacante: 0.25, CD_Ice: 0.60, CD_Ether: 0.60, MA: 40, Admg: 0.30},
@@ -12,12 +36,6 @@ const enemy = {
         buffs_enemy: { Res_Ether: 0.20, Res_Ice: 0.20, CD: 0.75, Admg: 0.45},
         buffs_stunned: {},
         Image: "static/ENEMY/Dead_End_Butcher.webp"
-    },
-    "Girtablullu": {
-        def_enemy: { Defense: 952 },
-        buffs_enemy: { Res_Wind: 0.2, Taken_Anomalo: 0.225},
-        buffs_stunned: { Taken_Anomalo: -0.225, Stun: 0.30 },
-        Image: "static/ENEMY/Girtablullu.webp"
     },
     "Sarah": {
         def_enemy: { Defense: 476 },
