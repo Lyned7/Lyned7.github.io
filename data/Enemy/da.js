7cc3453ed7eb2be4c2ed0de462aa659d32c6499b
Lyned <denilson.vasquez.lim@gmail.com>
2026-09-10 22:09:43 -0500

    Update enemy buffs with new properties and values

--- data/Enemy/da.js
+++ data/Enemy/da.js
@@ -1,18 +1,18 @@
 const da = {
     "[Buff 1]": {
-        buffs_da: {Dmg_Ultimate: 0.30, Dmg_Basic: 0.30, Res_Ice_Atacante: 0.20, Dmg_Ice: 0.30},
-        Image: "static/ENEMY/Buff1.webp"
+        buffs_da: {Penx: 0.05, Res_Electric: 0.15, Sdmg: 0.20, Deff: 0.10},
+        Image: "static/ENEMY/ArmorerBuff.webp"
     },
     "[Buff 2] - 2 Anomaly Agents": {
-        buffs_da: {Admg: 0.10, Atkf: 0.05, Res_Anomalo: 0.10, Shred_Anomalo: 0.10 },
+        buffs_da: {MA: 20, Shred: 0.10},
         Image: "static/ENEMY/Buff2.webp"
     },
     "[Buff 2] - 3 Anomaly Agents": {
-        buffs_da: {Admg: 0.30, Atkf: 0.15, Res_Anomalo: 0.10, Shred_Anomalo: 0.10},
+        buffs_da: {MA: 60, Shred: 0.10},
         Image: "static/ENEMY/Buff2.webp"
     },
     "[Buff 3]": {
-        buffs_da: {CD: 0.30, Atkf_Atacante: 0.10, Dmg_Basic: 0.30, Shred_Basic: 0.15},
+        buffs_da: {Dmg_Ice_Atacante: 0.20, CD: 0.30, Stun: 0.20},
         Image: "static/ENEMY/Buff3.webp"
     },
     
